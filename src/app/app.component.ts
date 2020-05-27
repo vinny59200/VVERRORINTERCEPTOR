59ec2d5692a6960b15ee1f504b55d686d24908cc
unknown <vvauban@gmail.com>
2020-05-28 00:20:54 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Movie } from './movie.model';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'VVERRORINTERCEPTOR';
+
+  private apiurl = "http://www.omdbapi.com/?apikey=faa771bf";
+
+  private badapiurl = "http://www.omdbapi.com/verybadurl";
+
+  currentMovie: Movie = new Movie({ 'title': "", 'plot': "", 'picture': "" });
+
+  constructor(private http: HttpClient) {
+    this.refreshMovie();
+  }
+
+  getData(title: string) {
+    return this.http.get(this.apiurl + "&t=" + title);
+  }
+
+  getDataWithBadURL(title: string) {
+    return this.http.get(this.badapiurl + "&t=" + title);
+  }
+
+  refreshMovie(): void {
+    this.getData(this.currentMovie.title).subscribe((data: any) => {
+      this.currentMovie.title = data.Title;
+      this.currentMovie.plot = data.Plot;
+      this.currentMovie.picture = data.Poster;
+    });
+  }
+
+  badRefreshMovie(): void {
+    this.getDataWithBadURL(this.currentMovie.title).subscribe((data: any) => {
+      this.currentMovie.title = data.Title;
+      this.currentMovie.plot = data.Plot;
+      this.currentMovie.picture = data.Poster;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpErrorInterceptor } from './http-error.interceptor';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,    
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: HttpErrorInterceptor,
+    multi: true,
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
